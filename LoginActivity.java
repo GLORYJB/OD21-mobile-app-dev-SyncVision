357e05eeb740e79ab978c22acfc18c96b0c5f740
GLORYJB <150887959+GLORYJB@users.noreply.github.com>
2024-05-15 08:43:56 +0300

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,26 @@
+package com.group5.group;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView registerLink = findViewById(R.id.registerlink);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
